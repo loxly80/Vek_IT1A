fbf2ab1d3348b58fcc3a85fc121d551d501567a0
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-10-26 09:22:33 +0200

    Add project files.

---
+++ Vek_IT1A/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Vek_IT1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+            Frame(10,10,"hello world");
+            Frame(10, 15, "pokus");
+            Frame(2, 2, "další rámeček");
+            Console.ReadLine();
+        }
+
+        static void Frame(int left, int top,string text)
+        {
+            Console.SetCursorPosition(left, top);
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("╔══════════════════════╗");
+            Console.SetCursorPosition(left, top + 1);
+            Console.Write("║");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("                      ");            
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("║");
+            Console.SetCursorPosition(left, top + 2);
+            Console.Write("╚══════════════════════╝");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(left + 1, top + 1);
+            Console.Write(text);
+        }
+    }
+}
